0eb696a23fae71014cf1518e285a226bf296ea41
Aldo Susanto <aldosusanto@hotmail.com>
2022-01-08 15:54:50 +0700

    [ALD-01] Handle Case when Backend Returns empty list (#2)
    * [ALD-01] Remove dummy commit
    
    * Add one extra line
    
    * [ALD-01] Handle case if Backend Returns empty list
    
    * Remove Debug Log

--- src/ResultPage/Result.js
+++ src/ResultPage/Result.js
@@ -4,7 +4,7 @@ import { Fragment } from 'react';
 import emailjs from 'emailjs-com';
 import Menubar from '../Menubar';
 import axios from 'axios';
-import { Button, Icon, Item } from 'semantic-ui-react'
+import { Button, Icon, Item, Label } from 'semantic-ui-react'
 import CurrencyFormat from 'react-currency-format';
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Loader from "react-loader-spinner";
@@ -58,87 +58,96 @@ class Result extends React.Component {
         var itemLinks = [];
 
         if(typeof(recList) !== 'undefined'){
-            // Populate Laptop List on left side of page
-            for(const[index, value] of recList.entries()){
-                itemList.push(
-                    <Item.Group divided >
-                        <Item className={`itemList ${index == this.state.highlightedIndex ? "selectedItem" : "" }`} onClick={(e) => this.changeHighlight(e, index)}>
-                            <Item.Image src={value.imageLink[0]} />
+            if(recList.length > 0){
+                // Populate Laptop List on left side of page
+                for(const[index, value] of recList.entries()){
+                    itemList.push(
+                        <Item.Group divided >
+                            <Item className={`itemList ${index == this.state.highlightedIndex ? "selectedItem" : "" }`} onClick={(e) => this.changeHighlight(e, index)}>
+                                <Item.Image src={value.imageLink[0]} />
+                                <Item.Content>
+                                    <Item.Header as='string'>{value.name}</Item.Header>
+                                    <Item.Meta>
+                                    <span className='price'><CurrencyFormat value={value.price} displayType={'text'} thousandSeparator={true} prefix={'Rp. '} /></span>
+                                    </Item.Meta>
+                                    <Item.Extra>
+                                    </Item.Extra>
+                                </Item.Content>
+                            </Item>
+                        </Item.Group>
+                    )
+                }
+
+                highlightedItem = recList[this.state.highlightedIndex];
+
+                // Populate the links(green button) for the highlightedItem
+                for(const[index, value] of highlightedItem.link.entries()){
+                    if(value.link.trim() != "" ){ //if link is empty
+                        // console.log("linkfrom", value.linkFrom);
+                        itemLinks.push(
+                            <a href={value.link} target="_blank">
+                            <Button floated='right' className={`item-desc-btn ${value.linkFrom}`}>
+                                {`Visit ${value.linkFrom}`}
+                                <Icon name='right chevron' />
+                            </Button>
+                        </a>
+                        );
+                    }
+                }
+
+                // Populate Laptop Description on right side of page
+                itemDescription.push(
+                    <Item.Group divided className="itemDesc">
+                        <Item>
                             <Item.Content>
-                                <Item.Header as='string'>{value.name}</Item.Header>
+                                <Item.Header as='a' className="title">{highlightedItem.name}</Item.Header>
                                 <Item.Meta>
-                                <span className='price'><CurrencyFormat value={value.price} displayType={'text'} thousandSeparator={true} prefix={'Rp. '} /></span>
+                                    <span className='price'><CurrencyFormat value={highlightedItem.price} displayType={'text'} thousandSeparator={true} prefix={'Rp. '} /></span>
                                 </Item.Meta>
+                                <Item.Description className="item-desc-box"><b>Processor: </b> {highlightedItem.processor} <br/> <b>RAM: </b> {highlightedItem.ram}GB <br /> <b>Graphics Card:</b> {highlightedItem.graphics} </Item.Description>
                                 <Item.Extra>
+                                <Label icon='gamepad' content='Powerful for gaming' />
+                                <Label icon='briefcase' content='Suitable for Travel' />
+                                {itemLinks}
                                 </Item.Extra>
                             </Item.Content>
                         </Item>
                     </Item.Group>
                 )
-            }
 
-            highlightedItem = recList[this.state.highlightedIndex];
-
-            // Populate the links(green button) for the highlightedItem
-            for(const[index, value] of highlightedItem.link.entries()){
-                if(value.link.trim() != "" ){ //if link is empty
-                    // console.log("linkfrom", value.linkFrom);
-                    itemLinks.push(
-                        <a href={value.link} target="_blank">
-                        <Button floated='right' className={`item-desc-btn ${value.linkFrom}`}>
-                            {`Visit ${value.linkFrom}`}
-                            <Icon name='right chevron' />
-                        </Button>
-                    </a>
-                    );
-                }
-            }
-
-            // Populate Laptop Description on right side of page
-            itemDescription.push(
-                <Item.Group divided className="itemDesc">
-                    <Item>
-                        <Item.Content>
-                            <Item.Header as='a' className="title">{highlightedItem.name}</Item.Header>
-                            <Item.Meta>
-                                <span className='price'><CurrencyFormat value={highlightedItem.price} displayType={'text'} thousandSeparator={true} prefix={'Rp. '} /></span>
-                            </Item.Meta>
-                            <Item.Description className="item-desc-box"><b>Processor: </b> {highlightedItem.processor} <br/> <b>RAM: </b> {highlightedItem.ram}GB <br /> <b>Graphics Card:</b> {highlightedItem.graphics} </Item.Description>
-                            <Item.Extra>
-                            {/* <Label icon='gamepad' content='Powerful for gaming' />
-                            <Label icon='briefcase' content='Suitable for Travel' />
-                            <Label icon='users' content='Highly Reviewed' /> */}
-                            {itemLinks}
-                            </Item.Extra>
-                        </Item.Content>
-                    </Item>
-                </Item.Group>
-            )
-
-            itemImage.push(
-                <img className="laptop-img" src={highlightedItem.imageLink[0]} alt="Highlighted Laptop"/>    
-            )
-            
-            // Compile all components 
-            resultsPage.push(
-                <div id="result" className="row result-home">
-                    <div className="col-lg-5 panel">
-                        <h1>Here are <br />Our top 10 picks for you</h1>
-                        <hr className="hr-line"/>
-                        <div className="result-text">
-                            {itemList}
+                itemImage.push(
+                    <img className="laptop-img" src={highlightedItem.imageLink[0]} alt="Highlighted Laptop"/>    
+                )
+                
+                // Compile all components 
+                resultsPage.push(
+                    <div id="result" className="row result-home">
+                        <div className="col-lg-5 panel">
+                            <h1>Here are <br />Our top 10 picks for you</h1>
+                            <hr className="hr-line"/>
+                            <div className="result-text">
+                                {itemList}
+                            </div>
+                        </div>
+                        <div className="col-lg-7">
+                            <div className="row">
+                                {itemImage}
+                            </div>
+                            <div className="row">
+                                {itemDescription}
+                            </div>
                         </div>
                     </div>
-                    <div className="col-lg-7">
-                        <div className="row">
-                            {itemImage}
-                        </div>
-                        <div className="row">
-                            {itemDescription}
-                        </div>
+                )
+            } else { // If BE returns empty list
+                resultsPage.push(
+                    <div className="row loading-page">
+                        <h1>Oops, ini memalukan :(</h1>
+                        <h2>Kami tidak berhasil menemukan laptop yang cocok dengan pilihanmu<br /> Coba mengulang quiznya lagi</h2>
+                        <p>Tip: Coba naikan budgetmu sedikit untuk memperluas pilihan kamu</p>
                     </div>
-                </div>
-            )
+                )
+            }
         }else {
             resultsPage.push(
                 <div className="row loading-page">
