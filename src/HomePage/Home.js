4bc4526d24ffabf64ccb384e7e532fa76e1d618e
Syawal Jasira <syawaljasira@gmail.com>
2022-01-11 22:24:34 +0700

    [SWL-02] add mobile responsive homepage

--- src/HomePage/Home.js
+++ src/HomePage/Home.js
@@ -16,65 +16,66 @@ const Home = () => (
       <title>ProPicks - Home</title>
     </Helmet>
     <Menubar />
-    {/* test pull request SWL-01 */}
 
     {/* Cover Page */}
-    <div id="home" className="row">
-      <div className="col-md-8 text-center">
-        <h1>Bingung cari laptop yang tepat ?</h1>
-        <p>
-          Isi pertanyaan berikut untuk mendapatkan rekomendasi<br></br>laptop
-          yang tepat untukmu
-        </p>
-        <Link to={`/play`}>
-          <button type="button" name="button" className="btn btn-success">
-            Cari Tahu
-          </button>
-        </Link>
+    <div className="main">
+      <div id="home" className="row">
+        <div className="col-md-8 text-center">
+          <h1>Bingung cari laptop yang tepat?</h1>
+          <p>
+            Isi pertanyaan berikut untuk mendapatkan rekomendasi
+            <br className="d-none d-sm-block"></br> laptop yang tepat untukmu
+          </p>
+          <Link to={`/play`}>
+            <button type="button" name="button" className="btn btn-success">
+              Cari Tahu
+            </button>
+          </Link>
+        </div>
+        <div className="col-sm-4 position-relative d-none d-md-block">
+          <img
+            className="fixed-bottom position-absolute bottom-0 end-0"
+            src={homeImage}
+            alt="ProPicks welcome cari laptop"
+          />
+        </div>
       </div>
-      <div className="col-sm-4 position-relative">
-        <img
-          className="fixed-bottom position-absolute bottom-0 end-0"
-          src={homeImage}
-          alt="ProPicks welcome cari laptop"
-        />
-      </div>
-    </div>
 
-    {/* How it works Page */}
-    <div className="howItWorks" id="howItWorks">
-      <div className="row HIW-row ">
-        <div className="col-md-12 text-center">
-          <h1>Cara Kerja</h1>
+      {/* How it works Page */}
+      <div className="howItWorks" id="howItWorks">
+        <div className="row HIW-row ">
+          <div className="col-md-12 text-center">
+            <h1>Cara Kerja</h1>
+          </div>
+        </div>
+        <div className="row HIW-row">
+          <div className="col-md-1"></div>
+          <div className="col-12 col-md-3 text-center mb-2 mb-md-0">
+            <h3 className="text-ce">Step 1</h3>
+            <img className="iconImage ml-3 ml-md-0" src={formIcon} alt=""></img>
+            <h3 className="HIW-title text-center">
+              Beritahu kebutuhan <br /> laptop kamu
+            </h3>
+          </div>
+          <div className="col-12 col-md-4 text-center my-5 my-md-0">
+            <h3 className="text-center">Step 2</h3>
+            <FontAwesomeIcon icon="search-dollar" className="HIW-icons" />
+            <h3 className="HIW-title text-center">
+              Kami rekomendasikan <br /> laptop terbaik untukmu
+            </h3>
+          </div>
+          <div className="col-12 col-md-3 text-center mt-2 mt-md-0">
+            <h3 className="text-center">Step 3</h3>
+            <img className="iconImage" src={smileIcon} alt=""></img>
+            <h3 className="HIW-title text-center">
+              Explore dan pilih <br /> laptop kesukaanmu
+            </h3>
+          </div>
+          <div className="col-md-1"></div>
         </div>
       </div>
-      <div className="row HIW-row">
-        <div className="col-md-1"></div>
-        <div className="col-md-3 text-center">
-          <h3 className="text-ce">Step 1</h3>
-          <img className="iconImage" src={formIcon} alt=""></img>
-          <h3 className="HIW-title text-center">
-            Beritahu kebutuhan <br /> laptop kamu
-          </h3>
-        </div>
-        <div className="col-md-4 text-center">
-          <h3 className="text-center">Step 2</h3>
-          <FontAwesomeIcon icon="search-dollar" className="HIW-icons" />
-          <h3 className="HIW-title text-center">
-            Kami rekomendasikan <br /> laptop terbaik untukmu
-          </h3>
-        </div>
-        <div className="col-md-3 text-center">
-          <h3 className="text-center">Step 3</h3>
-          <img className="iconImage" src={smileIcon} alt=""></img>
-          <h3 className="HIW-title text-center">
-            Explore dan pilih <br /> laptop kesukaanmu
-          </h3>
-        </div>
-        <div className="col-md-1"></div>
-      </div>
+      <Footer />
     </div>
-    <Footer />
   </Fragment>
 );
 
