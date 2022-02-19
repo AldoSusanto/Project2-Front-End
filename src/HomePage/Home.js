52d8496d6108fd0f9eb245dc0766a03dc39b4f14
syawaljasira <82795020+syawaljasira@users.noreply.github.com>
2022-02-19 08:56:28 +0700

    [SWL-05] Redesign home page with new layout (#13)
    * [SWL-05] Redesign homepage, make new UI Layout
    
    * [SWL-05] fix bugs SonarCloud, make reuse component headlineHighlight, add GTM script to used in the future
    
    * [SWL-05] fix bugs issue on GTM script
    
    * [SWL-05] add call to action button

--- src/HomePage/Home.js
+++ src/HomePage/Home.js
@@ -1,108 +1,234 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 import { Fragment } from "react";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import homeImage from "../assets/woman-laptop.png";
 import Menubar from "../Menubar";
 import Footer from "../Footer";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import formIcon from "../assets/form-icon-nobg.png";
 import smileIcon from "../assets/smile-nobg.png";
+import addInformationPng from "../assets/add-information.png";
+import programmingPng from "../assets/programming.png";
+import checkingBoxesPng from "../assets/checking-boxes.png";
+import partnersPng from "../assets/partners.png";
+import homeImg from "../assets/image-1.png";
 import { Popup } from "semantic-ui-react";
+import HeadlineHighlight from "../components/HeadlineHighlight";
 
 var prefix_url =
   "https://wa.me/6287868572240?text=Hello%20Propicks%2C%20saya%20ingin%20mencari%20laptop%20yang%20tepat%20untuk%20saya%20%21%20%3AD";
 
-const Home = () => (
-  <Fragment>
-    <Helmet>
-      <title>ProPicks - Home</title>
-    </Helmet>
-    <Menubar />
+const Home = () => {
+  const [isVisible, setIsVisible] = useState(0);
 
-    {/* Cover Page */}
-    <div className="main">
-      <div
-        class="alert alert-success promo-banner col-xs-1 text-center"
-        role="alert"
-      >
-        Konsultasi dengan kami via WA dan dapatkan cashback dari &nbsp;
-        <strong> Rp. 50,000 </strong> &nbsp; hingga &nbsp;
-        <strong> Rp. 300,000 !! </strong>
-      </div>
-      <div id="home" className="row">
-        <div className="col-12 col-md-8 text-center">
-          <h1>Bingung cari laptop yang tepat?</h1>
-          <p>
-            Isi pertanyaan berikut untuk mendapatkan rekomendasi
-            <br className="d-none d-sm-block"></br> laptop yang tepat untukmu
-          </p>
+  useEffect(() => {
+    if (isVisible >= 0 && isVisible !== 5) {
+      setTimeout(() => {
+        setIsVisible(isVisible + 1);
+      }, 2000);
+    } else {
+      setTimeout(() => {
+        setIsVisible(0);
+      }, 2000);
+    }
+  }, [isVisible]);
+
+  return (
+    <Fragment>
+      <Helmet>
+        <title>ProPicks - Home</title>
+      </Helmet>
+      <Menubar />
+
+      {/* Cover Page */}
+      <div className="main">
+        <div
+          className="alert alert-success position-absolute promo-banner col-12 text-center"
+          role="alert"
+        >
+          Konsultasi dengan kami via WA dan dapatkan cashback dari &nbsp;
+          <strong> Rp. 50,000 </strong> &nbsp; hingga &nbsp;
+          <strong> Rp. 300,000 !! </strong>
+        </div>
+        <div id="home" className="row">
+          <div className="col-12 col-md-6 col-xl-8 text-center">
+            <h1 className="headline">
+              Cari Laptop Untuk <br />
+              <HeadlineHighlight />
+            </h1>
+            <div className="d-block d-md-none">
+              <img
+                className="hero-img"
+                src={homeImg}
+                alt="ProPicks welcome cari laptop"
+              />
+            </div>
+            <p>
+              Propicks akan merekomendasikan laptop{" "}
+              <br className="d-none d-sm-block"></br> yang tepat untukmu
+            </p>
+            <Link to={`/play`}>
+              <button type="button" name="button" className="btn btn-success">
+                Mulai Quiz
+              </button>
+            </Link>
+          </div>
+          <div className="col-12 col-md-4 position-relative d-none d-md-block">
+            <img
+              className="hero-img position-absolute"
+              src={homeImg}
+              alt="ProPicks welcome cari laptop"
+            />
+          </div>
+        </div>
+
+        {/* How it works Page */}
+        <div className="howItWorks" id="howItWorks">
+          <div className="row HIW-row ">
+            <div className="col-md-12 text-center">
+              <h1>Cara Kerja</h1>
+            </div>
+          </div>
+          <div className="row HIW-row">
+            <div className="col-md-1"></div>
+            <div className="col-12 col-md-3 text-center mb-2 mb-md-0">
+              <h3 className="text-ce">Step 1</h3>
+              <img
+                className="iconImage ml-3 ml-md-0"
+                src={formIcon}
+                alt=""
+              ></img>
+              <h3 className="HIW-title text-center">
+                Beritahu kebutuhan <br /> laptop kamu
+              </h3>
+            </div>
+            <div className="col-12 col-md-4 text-center my-5 my-md-0">
+              <h3 className="text-center">Step 2</h3>
+              <FontAwesomeIcon icon="search-dollar" className="HIW-icons" />
+              <h3 className="HIW-title text-center">
+                Kami rekomendasikan <br /> laptop terbaik untukmu
+              </h3>
+            </div>
+            <div className="col-12 col-md-3 text-center mt-2 mt-md-0">
+              <h3 className="text-center">Step 3</h3>
+              <img className="iconImage" src={smileIcon} alt=""></img>
+              <h3 className="HIW-title text-center">
+                Explore dan pilih <br /> laptop kesukaanmu
+              </h3>
+            </div>
+            <div className="col-md-1"></div>
+          </div>
+        </div>
+
+        {/* Partner Propicks Page */}
+        <div className="partner-propicks row">
+          <div className="col-12 col-lg-6 partner-propicks-info">
+            <h5>Partner Propicks</h5>
+            <p>
+              Propicks telah membangun kerja sama dengan beberapa partner
+              terpercaya agar kamu dapat mencari laptop dengan aman dan nyaman.
+            </p>
+          </div>
+          <div className="col-12 col-lg-6 partner-propicks-img">
+            <img src={partnersPng} alt="" />
+          </div>
+        </div>
+
+        {/* About Page */}
+        <div className="about">
+          <h5 className="">
+            <FontAwesomeIcon icon="search" className="icon-search" />
+            Yuk mengenal lebih dalam tentang Propicks
+          </h5>
+          <div className="row about-row">
+            <div className="col-12 col-lg-6 about-header d-flex flex-column-reverse flex-lg-column justify-content-center align-items-center">
+              <h4>
+                Apa itu <span className="border-blue">Propicks?</span>
+              </h4>
+              <img src={addInformationPng} alt="" />
+            </div>
+            <div className="col-12 col-lg-6 about-description">
+              <p>
+                Propicks adalah quiz tentang aktivitas harian kamu untuk mencari
+                laptop/notebook yang paling bagus, murah dan terbaik untukmu.
+                Cukup isi quiz kami dan sistem kami akan memberikan rekomendasi
+                laptop yang tepat untukmu. Tidak perlu membuat akun dan tidak
+                ada data yang dilacak karena anda tidak memberikan informasi
+                kontak apapun.
+              </p>
+            </div>
+          </div>
+          <div className="row about-row flex-column-reverse flex-lg-row">
+            <div className="col-12 col-lg-6 about-description">
+              <p>
+                Propicks terinspirasi dari banyaknya pelajar, gamers, desainer,
+                editor yang cenderung membeli laptop/notebook yang tidak cocok
+                untuk keperluan mereka. Banyak dari mereka yang membeli laptop
+                yang terlalu mahal, performa kurang bagus, kurang ringan, atau
+                tidak tahan lama. Propicks bertujuan untuk membantu kamu mencari
+                dan merekomendasikan laptop tipe apapun dengan harga laptop yang
+                murah dan lebih terjangkau. Laptop yang dapat ditujukan untuk
+                desainer, laptop untuk editor, laptop gaming murah terbaik, dan
+                lain-lain.
+              </p>
+            </div>
+            <div className="col-12 col-lg-6 about-header d-flex flex-column-reverse flex-lg-column justify-content-center align-items-center">
+              <h4>
+                Visi <span className="border-red">Propicks</span>
+              </h4>
+              <img src={programmingPng} alt="" />
+            </div>
+          </div>
+          <div className="row about-row">
+            <div className="col-12 col-lg-6 about-header d-flex flex-column-reverse flex-lg-column justify-content-center align-items-center">
+              <h4 className="text-center">
+                Rekomendasi & Review{" "}
+                <span className="border-green">Propicks</span>
+              </h4>
+              <img src={checkingBoxesPng} alt="" />
+            </div>
+            <div className="col-12 col-lg-6 about-description">
+              <p>
+                Propicks mempunyai banyak rekomendasi dan review terbaik untuk
+                banyak laptop: Lenovo Ideapad, Lenovo Thinkpad, Lenovo Legion,
+                Lenovo Yoga Slim, HP Pavillion Gaming, HP 14s, Asus Vivobook.
+                Asus Zenbook, Asus ROG Strix. Asus a409, Asus TUF Gaming, Acer
+                Nitro, Acer Aspire, Dell Inspiron, Dell Vostro, Dell XPS, MSI
+                GF63, MSI GF65, MSI Prestige, MSI Stealth, Xiaomi Redmibook,
+                Infinix Inbook, Axioo, Mybook dan lain-lain.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Call To Action */}
+        <div className="call-to-action d-flex flex-column flex-md-row justify-content-center align-items-center">
+          <h5>Sudah siap mencari laptop impianmu?</h5>
           <Link to={`/play`}>
             <button type="button" name="button" className="btn btn-success">
-              Cari Tahu
+              Mulai Quiz
             </button>
           </Link>
         </div>
-        <div className="col-sm-4 position-relative d-none d-md-block">
-          <img
-            className="fixed-bottom position-absolute bottom-0 end-0"
-            src={homeImage}
-            alt="ProPicks welcome cari laptop"
-          />
-        </div>
+        <Footer />
+        <Popup
+          inverted
+          content="Dapatkan konsultasi gratis dari tim professional kami dari Whatsapp !"
+          trigger={
+            <a
+              href={prefix_url}
+              className="whatsapp-float"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fa fa-whatsapp whatsapp-icon"></i>
+            </a>
+          }
+        />
       </div>
-
-      {/* How it works Page */}
-      <div className="howItWorks" id="howItWorks">
-        <div className="row HIW-row ">
-          <div className="col-md-12 text-center">
-            <h1>Cara Kerja</h1>
-          </div>
-        </div>
-        <div className="row HIW-row">
-          <div className="col-md-1"></div>
-          <div className="col-12 col-md-3 text-center mb-2 mb-md-0">
-            <h3 className="text-ce">Step 1</h3>
-            <img className="iconImage ml-3 ml-md-0" src={formIcon} alt=""></img>
-            <h3 className="HIW-title text-center">
-              Beritahu kebutuhan <br /> laptop kamu
-            </h3>
-          </div>
-          <div className="col-12 col-md-4 text-center my-5 my-md-0">
-            <h3 className="text-center">Step 2</h3>
-            <FontAwesomeIcon icon="search-dollar" className="HIW-icons" />
-            <h3 className="HIW-title text-center">
-              Kami rekomendasikan <br /> laptop terbaik untukmu
-            </h3>
-          </div>
-          <div className="col-12 col-md-3 text-center mt-2 mt-md-0">
-            <h3 className="text-center">Step 3</h3>
-            <img className="iconImage" src={smileIcon} alt=""></img>
-            <h3 className="HIW-title text-center">
-              Explore dan pilih <br /> laptop kesukaanmu
-            </h3>
-          </div>
-          <div className="col-md-1"></div>
-        </div>
-      </div>
-      <Footer />
-      <Popup
-        inverted
-        content="Dapatkan konsultasi gratis dari tim professional kami dari Whatsapp !"
-        trigger={
-          <a
-            href={prefix_url}
-            className="whatsapp-float"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fa fa-whatsapp whatsapp-icon"></i>
-          </a>
-        }
-      />
-    </div>
-  </Fragment>
-);
+    </Fragment>
+  );
+};
 
 export default Home;
--- src/Menubar.js
+++ src/Menubar.js
@@ -1,79 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { Nav } from "reactstrap";
 import logoImage from "./assets/Logo/Original-Transparent.png";
 import logoSm from "./assets/Logo/Symbol.png";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { MdOutlineMenu, MdOutlineClose } from "react-icons/md";
 
-class Menubar extends React.Component {
-  constructor(props) {
-    super();
-    this.state = {
-      courses: [],
-      reload: false,
-      showNav: false,
-    };
-  }
+const Menubar = (props) => {
+  const [showNav, setShowNav] = useState(false);
 
-  render() {
-    return (
-      <Nav className="navbar navbar-expand-md navbar-light menubarStyle fixed-top">
-        <div className="container-fluid">
-          <a href="/">
-            <img
-              href="/"
-              className="logo d-none d-md-block ml-3 ml-md-5"
-              src={logoImage}
-              alt="Propicks logo"
-            />
-            <img
-              href="/"
-              className="logoSm d-block d-md-none ml-3 ml-md-5"
-              src={logoSm}
-              alt="Propicks logo"
-            />
-          </a>
-          <div className="navbar-collapse d-none d-md-block">
-            <div className="navbar-nav">
-              <a className="menuBtn" href="/#howItWorks">
-                Cara Kerja
-              </a>
-              <a className="menuBtn" href="/play">
-                Mulai Quiz
-              </a>
-            </div>
-          </div>
-          <div
-            onClick={() => this.setState({ showNav: !this.state.showNav })}
-            className="d-md-none d-block"
-          >
-            <MdOutlineMenu size="100%" className="menu-icons" />
-          </div>
-          <div
-            className={`offcanvas d-md-none d-block ${
-              this.state.showNav ? "show-nav" : "hidden-nav"
-            }`}
-          >
-            <div className="offcanvas__nav d-flex flex-column justify-content-center pl-1">
-              <span
-                className="offcanvas__close align-self-end"
-                href="/#howItWorks"
-                onClick={() => this.setState({ showNav: !this.state.showNav })}
-              >
-                <MdOutlineClose size="100%" className="close-icons" />
-              </span>
-              <a className="offcanvas__link" href="/#howItWorks">
-                Cara Kerja
-              </a>
-              <a className="offcanvas__link" href="/play">
-                Mulai Quiz
-              </a>
-            </div>
+  return (
+    <Nav
+      className={`navbar navbar-expand-md navbar-light menubarStyle fixed-top bgBlue`}
+    >
+      <div className="container-fluid">
+        <a href="/">
+          <img
+            href="/"
+            className="logo d-none d-md-block ml-3 ml-md-5"
+            src={logoImage}
+            alt="Propicks logo"
+          />
+          <img
+            href="/"
+            className="logoSm d-block d-md-none ml-3 ml-md-5"
+            src={logoSm}
+            alt="Propicks logo"
+          />
+        </a>
+        <div className="navbar-collapse d-none d-md-block">
+          <div className="navbar-nav">
+            <a className="menuBtn" href="/#howItWorks">
+              Cara Kerja
+            </a>
+            <a className="menuBtn" href="/play">
+              Mulai Quiz
+            </a>
           </div>
         </div>
-      </Nav>
-    );
-  }
-}
+        <div onClick={() => setShowNav(!showNav)} className="d-md-none d-block">
+          <MdOutlineMenu size="100%" className="menu-icons" />
+        </div>
+        <div
+          className={`offcanvas d-md-none d-block ${
+            showNav ? "show-nav" : "hidden-nav"
+          }`}
+        >
+          <div className="offcanvas__nav d-flex flex-column justify-content-center pl-1">
+            <span
+              className="offcanvas__close align-self-end"
+              href="/#howItWorks"
+              onClick={() => setShowNav(!showNav)}
+            >
+              <MdOutlineClose size="100%" className="close-icons" />
+            </span>
+            <a className="offcanvas__link" href="/#howItWorks">
+              Cara Kerja
+            </a>
+            <a className="offcanvas__link" href="/play">
+              Mulai Quiz
+            </a>
+          </div>
+        </div>
+      </div>
+    </Nav>
+  );
+};
 
 export default Menubar;
