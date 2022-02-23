142ab4a27aca36a1aae0b06596519374e6ab6223
syawaljasira <syawaljasira12@gmail.com>
2022-02-23 09:43:40 +0700

    [SWL-06] refactoring play page, add icon to multiple option

--- src/PlayPage/Play.js
+++ src/PlayPage/Play.js
@@ -1,13 +1,13 @@
-import React, { Component } from "react";
-import { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
+import Menubar from "../Menubar";
 import jsonQuestions from "../questions.json";
 import classNames from "classnames";
-import Menubar from "../Menubar";
 import emailjs from "emailjs-com";
+import { MdCheckCircleOutline } from "react-icons/md";
 
-class Play extends Component {
-  resultJson = {
+const Play = (props) => {
+  let resultJson = {
     priceRange: "",
     pricePref: "medium",
     activities: [],
@@ -39,138 +39,29 @@ class Play extends Component {
     brand: [],
   };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      questions: jsonQuestions,
-      currentQuestion: jsonQuestions[0],
-      nextQuestion: {},
-      previousQuestion: {},
-      answer: "",
-      currentQuestionIndex: 0,
-      isFirstQuestion: true,
-      collectedTags: [],
-      currentSelectedTags: [],
-      currentSelectedChoices: [],
-      result: this.resultJson,
-    };
+  const [currentQuestion, setCurrentQuestion] = useState(jsonQuestions[0]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [collectedTags, setCollectedTags] = useState([]);
+  const [currentSelectedTags, setCurrentSelectedTags] = useState([]);
+  const [currentSelectedChoices, setCurrentSelectedChoices] = useState([]);
+  const [result, setResult] = useState(resultJson);
 
-    this.userAnswered = this.userAnswered.bind(this);
-    this.buttonClick = this.buttonClick.bind(this);
-  }
+  // Deprecated
+  const [questions, setQuestions] = useState(jsonQuestions);
+  const [isFirstQuestion, setIsFirstQuestion] = useState(true);
+  const [nextQuestion, setNextQuestion] = useState({});
+  const [previousQuestion, setPreviousQuestion] = useState({});
+  const [answer, setAnswer] = useState("");
 
-  render() {
-    const { currentQuestion } = this.state;
-    const choices = this.generateOptions(currentQuestion);
-
-    return (
-      <Fragment>
-        <Helmet>
-          <title>ProPicks - Laptop Quiz</title>
-        </Helmet>
-        <Menubar />
-        <div className="quiz-body">
-          <div className="questions">
-            <h5>{currentQuestion.question}</h5>
-
-            {choices}
-            <div className="button-container d-flex justify-content-center justify-content-md-start">
-              <button
-                id="next-button"
-                onClick={this.buttonClick}
-                className={classNames("", {
-                  disable: this.state.currentSelectedTags.length <= 0,
-                })}
-              >
-                NEXT
-              </button>
-            </div>
-          </div>
-        </div>
-      </Fragment>
-    );
-  }
-
-  componentDidMount() {
-    const { currentQuestionIndex } = this.state;
-
-    this.setState({
-      currentQuestion: jsonQuestions[currentQuestionIndex],
-      answer: jsonQuestions[currentQuestionIndex].answer,
-    });
-  }
-
-  disableButtonIfNeeded(nextIndex) {
+  function disableButtonIfNeeded(nextIndex) {
     if (nextIndex > 0) {
-      this.setState({
-        isFirstQuestion: false,
-      });
+      setIsFirstQuestion(false);
     } else {
-      this.setState({
-        isFirstQuestion: true,
-      });
+      setIsFirstQuestion(true);
     }
   }
 
-  buttonClick(event) {
-    var index = this.state.currentQuestionIndex;
-
-    switch (event.target.id) {
-      case "prev-button": // Deprecated
-        if (index > 0) {
-          this.setState({
-            currentQuestion: jsonQuestions[index - 1],
-            currentQuestionIndex: index - 1,
-          });
-        }
-        this.disableButtonIfNeeded(index - 1);
-        break;
-      case "next-button":
-        //1) Gather the choices that user has selected
-        var tagArray = this.state.collectedTags.concat(
-          this.state.currentSelectedTags
-        );
-        //1.5) Check whether user has selected a choice
-
-        var nextIndex = this.determineNextIndex(tagArray, jsonQuestions, index);
-
-        var currentTagList = this.state.currentSelectedTags;
-        var questionLabel = this.state.currentQuestion.questionLabel;
-
-        //2) For each of the choices, map them to the correct result
-        for (const [_, value] of currentTagList.entries()) {
-          this.mapAnswerToResult(questionLabel, value);
-        }
-
-        //3) Move on to the next question and refresh the "currentSelectedTags" variable
-        if (index < jsonQuestions.length - 1) {
-          this.setState({
-            currentQuestion: jsonQuestions[nextIndex],
-            currentQuestionIndex: nextIndex,
-            collectedTags: tagArray,
-            currentSelectedTags: [],
-          });
-        } else {
-          // 4) If we reached the end of questions list, we show the result page
-          console.log(JSON.stringify(this.state.result));
-          this.sendEmail();
-          this.props.history.push({
-            pathname: "/result",
-            state: this.state.result,
-          });
-        }
-        // console.log("CollectedTags: " + JSON.stringify(tagArray));
-        this.disableButtonIfNeeded(index + 1);
-        break;
-      default:
-        console.error(
-          "A button is clicked, but the ID is unidentified " + event.target.id
-        );
-        break;
-    }
-  }
-
-  determineNextIndex(tagArray, jsonQuestions, index) {
+  function determineNextIndex(tagArray, jsonQuestions, index) {
     if (index >= jsonQuestions.length - 1) {
       return index + 1;
     }
@@ -186,138 +77,22 @@ class Play extends Component {
     }
   }
 
-  generateOptions(currentQuestion) {
-    var choices = [];
+  function userAnswered(event, tagIndex) {
+    var selectedTag = currentQuestion.tags[tagIndex];
+    var indexAndQuestionLabel = currentQuestion.questionLabel + tagIndex;
 
-    let questions = currentQuestion.options;
-
-    if (!currentQuestion.isMultiple) {
-      for (var i = 0; i < questions.length; i = i + 2) {
-        if (i + 1 < questions.length) {
-          let q1 = i;
-          let q2 = i + 1;
-
-          let q1Selected = this.state.currentSelectedChoices.includes(
-            currentQuestion.questionLabel + q1
-          );
-          let q2Selected = this.state.currentSelectedChoices.includes(
-            currentQuestion.questionLabel + q2
-          );
-
-          choices.push(
-            <div key={i} className="options-container">
-              <p
-                key={questions[i]}
-                onClick={(e) => this.userAnswered(e, q1)}
-                className={q1Selected ? "option selected" : "option"}
-              >
-                {questions[i]}
-              </p>
-              <p
-                key={questions[i + 1]}
-                onClick={(e) => this.userAnswered(e, q2)}
-                className={q2Selected ? "option selected" : "option"}
-              >
-                {questions[i + 1]}
-              </p>
-            </div>
-          );
-        } else {
-          let q1 = i;
-          let q1Selected = this.state.currentSelectedChoices.includes(
-            currentQuestion.questionLabel + q1
-          );
-          choices.push(
-            <div key={i} className="options-container">
-              <p
-                key={questions[i]}
-                onClick={(e) => this.userAnswered(e, q1)}
-                className={q1Selected ? "option selected" : "option"}
-              >
-                {questions[i]}
-              </p>
-            </div>
-          );
-        }
-      }
-    } else {
-      for (i = 0; i < questions.length; i = i + 2) {
-        if (i + 1 < questions.length) {
-          let q1 = i;
-          let q2 = i + 1;
-
-          let q1Selected = this.state.currentSelectedChoices.includes(
-            currentQuestion.questionLabel + q1
-          );
-          let q2Selected = this.state.currentSelectedChoices.includes(
-            currentQuestion.questionLabel + q2
-          );
-
-          choices.push(
-            <div key={i} className="options-container">
-              <p
-                key={questions[i]}
-                onClick={(e) => this.userAnsweredMultiple(e, q1)}
-                className={
-                  q1Selected ? "option-multiple selected" : "option-multiple"
-                }
-              >
-                {questions[i]}
-              </p>
-              <p
-                key={questions[i + 1]}
-                onClick={(e) => this.userAnsweredMultiple(e, q2)}
-                className={
-                  q2Selected ? "option-multiple selected" : "option-multiple"
-                }
-              >
-                {questions[i + 1]}
-              </p>
-            </div>
-          );
-        } else {
-          let q1 = i;
-          let q1Selected = this.state.currentSelectedChoices.includes(
-            currentQuestion.questionLabel + q1
-          );
-
-          choices.push(
-            <div key={i} className="options-container">
-              <p
-                key={questions[i]}
-                onClick={(e) => this.userAnsweredMultiple(e, q1)}
-                className={
-                  q1Selected ? "option-multiple selected" : "option-multiple"
-                }
-              >
-                {questions[i]}
-              </p>
-            </div>
-          );
-        }
-      }
-    }
-    return choices;
+    setCurrentSelectedTags([selectedTag]);
+    setCurrentSelectedChoices([indexAndQuestionLabel]);
   }
 
-  userAnswered(event, tagIndex) {
-    var selectedTag = this.state.currentQuestion.tags[tagIndex];
-    var indexAndQuestionLabel =
-      this.state.currentQuestion.questionLabel + tagIndex;
-    this.setState({
-      currentSelectedTags: [selectedTag],
-      currentSelectedChoices: [indexAndQuestionLabel],
-    });
-  }
-
-  userAnsweredMultiple(event, tagIndex) {
+  function userAnsweredMultiple(event, tagIndex) {
     // 1) Grab the index of the multiple choice that user just clicked, then find the tag for it
-    var selectedTag = this.state.currentQuestion.tags[tagIndex];
-    var tagArray = this.state.currentSelectedTags;
+    var selectedTag = currentQuestion.tags[tagIndex];
+    var tagArray = currentSelectedTags;
 
     //2) We check whether the tag selected already exists in our currentSelectedTags array or not. (-1 if not found)
     const tagExists = (element) => element === selectedTag;
-    var findTagIndex = tagArray.findIndex(tagExists);
+    var findTagIndex = currentSelectedTags.findIndex(tagExists);
 
     if (findTagIndex >= 0) {
       // console.log(selectedTag + " has already been selected, removing element from index: " + findTagIndex);
@@ -331,9 +106,8 @@ class Play extends Component {
     // CurrentSelectedTags contains elements like: videoConference, watchFilm
     // The consistent naming in choices makes it easier for us to identify the selected choices.
     // Do a console.log on the choices and tags array in render func if you don't understand
-    var choicesArray = this.state.currentSelectedChoices;
-    var indexAndQuestionLabel =
-      this.state.currentQuestion.questionLabel + tagIndex;
+    var choicesArray = currentSelectedChoices;
+    var indexAndQuestionLabel = currentQuestion.questionLabel + tagIndex;
     const choiceExists = (element) => element === indexAndQuestionLabel;
     var findChoicesIndex = choicesArray.findIndex(choiceExists); // We check whether the choice selected already exists in our array or not. (-1 if not found)
 
@@ -343,14 +117,12 @@ class Play extends Component {
       choicesArray = choicesArray.concat(indexAndQuestionLabel);
     }
 
-    this.setState({
-      currentSelectedTags: tagArray,
-      currentSelectedChoices: choicesArray,
-    });
+    setCurrentSelectedTags([...tagArray]);
+    setCurrentSelectedChoices([...choicesArray]);
   }
 
-  mapAnswerToResult(questionLabel, selectedTag) {
-    var currResult = this.state.result;
+  function mapAnswerToResult(questionLabel, selectedTag) {
+    var currResult = result;
 
     if (questionLabel === "priceRange") {
       currResult.priceRange = selectedTag;
@@ -420,14 +192,12 @@ class Play extends Component {
       currResult.brand = currResult.brand.concat(selectedTag);
     }
 
-    this.setState({
-      result: currResult,
-    });
+    setResult(currResult);
   }
 
-  sendEmail() {
+  function sendEmail() {
     var emailBody = {
-      result: JSON.stringify(this.state.result, null, "\t"),
+      result: JSON.stringify(result, null, "\t"),
     };
 
     emailjs
@@ -446,6 +216,132 @@ class Play extends Component {
         }
       );
   }
-}
+
+  const handleNext = () => {
+    //1) Gather the choices that user has selected
+    var tagArray = collectedTags.concat(currentSelectedTags);
+    //1.5) Check whether user has selected a choice
+
+    var nextIndex = determineNextIndex(
+      tagArray,
+      jsonQuestions,
+      currentQuestionIndex
+    );
+
+    var currentTagList = currentSelectedTags;
+    var questionLabel = currentQuestion.questionLabel;
+
+    //2) For each of the choices, map them to the correct result
+    for (const [_, value] of currentTagList.entries()) {
+      mapAnswerToResult(questionLabel, value);
+    }
+
+    //3) Move on to the next question and refresh the "currentSelectedTags" variable
+    if (currentQuestionIndex < jsonQuestions.length - 1) {
+      setCurrentQuestion(jsonQuestions[nextIndex]);
+      setCurrentQuestionIndex(nextIndex);
+      setCollectedTags(tagArray);
+      setCurrentSelectedTags([]);
+    } else {
+      // 4) If we reached the end of questions list, we show the result page
+      console.log(JSON.stringify(result));
+      sendEmail();
+      props.history.push({
+        pathname: "/result",
+        state: result,
+      });
+    }
+    // console.log("CollectedTags: " + JSON.stringify(tagArray));
+    disableButtonIfNeeded(currentQuestionIndex + 1);
+  };
+
+  useEffect(() => {
+    setCurrentQuestion(jsonQuestions[currentQuestionIndex]);
+    setAnswer(jsonQuestions[currentQuestionIndex].answer);
+  }, [currentQuestionIndex]);
+
+  return (
+    <Fragment>
+      <Helmet>
+        <title>ProPicks - Laptop Quiz</title>
+      </Helmet>
+      <Menubar />
+      <div className="quiz-body">
+        <div className="questions">
+          <h5>
+            {currentQuestion.question}
+            <br />
+            {currentQuestion.isMultiple && (
+              <span className="questions-multiple">Pilih Multiple</span>
+            )}
+          </h5>
+
+          {!currentQuestion.isMultiple ? (
+            // currentQuestion.askIf ===
+            <>
+              {currentQuestion.options.map((option, i) => {
+                let optionSelected = currentSelectedChoices.includes(
+                  currentQuestion.questionLabel + i
+                );
+
+                return (
+                  <div key={i} className="options-container">
+                    <p
+                      onClick={(e) => userAnswered(e, i)}
+                      className={optionSelected ? "option selected" : "option"}
+                    >
+                      {option}
+                    </p>
+                  </div>
+                );
+              })}
+            </>
+          ) : (
+            <>
+              {currentQuestion.options.map((option, i) => {
+                let optionSelected = currentSelectedChoices.includes(
+                  currentQuestion.questionLabel + i
+                );
+
+                return (
+                  <div key={i} className="options-container">
+                    <p
+                      key={option[i]}
+                      onClick={(e) => userAnsweredMultiple(e, i)}
+                      className={
+                        optionSelected
+                          ? "option-multiple selected"
+                          : "option-multiple"
+                      }
+                    >
+                      <span
+                        className={optionSelected ? "icon-check" : "d-none"}
+                      >
+                        <MdCheckCircleOutline />
+                      </span>
+                      {option}
+                    </p>
+                  </div>
+                );
+              })}
+            </>
+          )}
+          <div className="button-container d-flex justify-content-center justify-content-md-start">
+            <button
+              id="next-button"
+              onClick={handleNext}
+              className={classNames(
+                "",
+                currentSelectedTags.length === 0 && "disable"
+              )}
+            >
+              NEXT
+            </button>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
 
 export default Play;
