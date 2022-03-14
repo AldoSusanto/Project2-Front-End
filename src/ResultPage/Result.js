a0d48e1116eac61e58594b1cd6ca40c75bed8c5a
Aldo Susanto <aldosusanto@hotmail.com>
2022-03-14 20:31:55 +0700

    [ALD-11] Refactor and small things

--- src/ResultPage/Result.js
+++ src/ResultPage/Result.js
@@ -63,6 +63,7 @@ class Result extends React.Component {
     var prefix_wa =
       "Hello Propicks, saya ingin mencari laptop yang tepat untuk saya ! Code:\n\n";
     var prefix_url = "https://wa.me/6287868572240?text=";
+    var storageDesc = [];
 
     if (typeof recList !== "undefined") {
       // Encrypt
@@ -165,10 +166,10 @@ class Result extends React.Component {
                   <b>Processor: </b> {highlightedItem.processor} <br />{" "}
                   <b>RAM: </b> {`${highlightedItem.ram} GB`} <br />{" "}
                   <b>Storage:</b> {`${highlightedItem.storageOne} GB`} <br />{" "}
-                  <b>Graphics Card:</b> {highlightedItem.graphics}
+                  <b>Graphics:</b> {highlightedItem.graphics}
                   <br /> <b>
-                    Display Size:
-                  </b> {`${highlightedItem.size} inches`} <br /> <b>Weight:</b>{" "}
+                    Display 
+                  </b> {`${highlightedItem.size} "`} <br /> <b>Weight:</b>{" "}
                   {`${highlightedItem.weightGrams} kg`}{" "}
                 </Item.Description>
                 <Item.Extra className="d-flex align-items-center justify-content-center">
